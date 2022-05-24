f7f5a7bd37bdd35e074e424e1157fd562be6d46e
psga <pusugag@unal.edu.co>
2022-05-24 15:13:36 +0000

    1

--- src/test/java/test/TestJugador.java
+++ src/test/java/test/TestJugador.java
@@ -7,12 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import futbol.Futbolista;
 import futbol.Jugador;
 
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
